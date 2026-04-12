38be1cde60ce009251eae72330a4da79fc5aeddf
Leonardo Oliveira <71794931+LeonardoOliveira96@users.noreply.github.com>
2026-04-12 01:18:54 -0300

    ci: configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode: _mode }) => ({
+  base: "/ConverterAuto/",
   server: {
     host: "::",
     port: 8080,
